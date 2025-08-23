eef242e2d16c4321ea12248a37022b222619fff7
367 <367@tuta.io>
2025-08-23 15:33:15 -0500

    +type byte_slice[T any] []byte

--- chatBot.go
+++ chatBot.go
@@ -2,26 +2,16 @@ package chatBot
 
 import (
    "encoding/json"
+   "fmt"
    "io"
    "net/http"
-   "strconv"
 )
 
-type model struct {
-   Author string
-   ShortName string `json:"short_name"`
-   Slug string
-}
+type byte_slice[T any] []byte
 
-type bytes[T any] []byte
-
-// 128000
-func get_models(content int) (bytes[models], error) {
+func get_models() (byte_slice[models], error) {
    req, _ := http.NewRequest("", "https://openrouter.ai", nil)
    req.URL.Path = "/api/frontend/models/find"
-   if content >= 1 {
-      req.URL.RawQuery = "context=" + strconv.Itoa(content)
-   }
    resp, err := http.DefaultClient.Do(req)
    if err != nil {
       return nil, err
@@ -30,12 +20,50 @@ func get_models(content int) (bytes[models], error) {
    return io.ReadAll(resp.Body)
 }
 
-type models struct {
-   Data struct {
-      Models []model
+func (m *model) String() string {
+   var b []byte
+   b = fmt.Appendln(b, "slug =", m.Slug)
+   b = fmt.Appendln(b, "author =", m.Author)
+   b = fmt.Appendln(b, "short name =", m.ShortName)
+   b = fmt.Append(b, "context length = ", m.ContextLength)
+   if m.Endpoint != nil {
+      b = fmt.Append(b, "\ncompletion = ", m.Endpoint.Pricing.Completion)
+      b = fmt.Append(b, "\nprompt = ", m.Endpoint.Pricing.Prompt)
+   }
+   return string(b)
+}
+
+type model struct {
+   Slug string
+   Author string
+   ShortName string `json:"short_name"`
+   ContextLength int `json:"context_length"` // DELETE
+   Endpoint *struct { // DELETE
+      Pricing struct {
+         Completion price `json:",string"`
+         Prompt price `json:",string"`
+      }
    }
 }
 
-func (m *models) unmarshal(data bytes[models]) error {
-   return json.Unmarshal(data, m)
+type models []*model
+
+func (m *models) unmarshal(data byte_slice[models]) error {
+   var value struct {
+      Data struct {
+         Models []*model
+      }
+   }
+   err := json.Unmarshal(data, &value)
+   if err != nil {
+      return err
+   }
+   *m = value.Data.Models
+   return nil
 }
+
+func (p price) String() string {
+   return fmt.Sprintf("$%.2f/M", float64(p) * 1_000_000)
+}
+
+type price float64
--- chatBot_test.go
+++ chatBot_test.go
@@ -1,16 +1,69 @@
 package chatBot
 
 import (
+   "bytes"
+   "encoding/json"
+   "fmt"
+   "log"
    "os"
+   "slices"
    "testing"
 )
 
-func TestMarshal(t *testing.T) {
-   data, err := get_models(0)
+func TestUnmarshal(t *testing.T) {
+   data, err := os.ReadFile("ignore/chatBot.json")
    if err != nil {
       t.Fatal(err)
    }
-   err = os.WriteFile("ignore/chatBot.json", data, os.ModePerm)
+   var modelsVar models
+   err = modelsVar.unmarshal(data)
+   if err != nil {
+      t.Fatal(err)
+   }
+   modelsVar = slices.DeleteFunc(modelsVar, func(m *model) bool {
+      if m.ContextLength < 128000 {
+         return true
+      }
+      if m.Endpoint == nil {
+         return true
+      }
+      return false
+   })
+   for _, slug := range good_slugs {
+      i := slices.IndexFunc(modelsVar, func(m *model) bool {
+         return m.Slug == slug
+      })
+      if i == -1 {
+         t.Fatal(slug)
+      }
+   }
+   for _, modelVar := range modelsVar {
+      fmt.Print(modelVar, "\n\n")
+   }
+   log.Print(len(modelsVar))
+}
+
+var good_slugs = []string{
+   "anthropic/claude-opus-4.1",
+   "anthropic/claude-sonnet-4",
+   "google/gemini-2.5-flash",
+   "google/gemini-2.5-pro",
+   "openai/gpt-4o",
+   "openai/gpt-5",
+   "qwen/qwen3-coder",
+}
+
+func TestMarshal(t *testing.T) {
+   data, err := get_models()
+   if err != nil {
+      t.Fatal(err)
+   }
+   var data1 bytes.Buffer
+   err = json.Indent(&data1, data, "", " ")
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = os.WriteFile("ignore/chatBot.json", data1.Bytes(), os.ModePerm)
    if err != nil {
       t.Fatal(err)
    }
