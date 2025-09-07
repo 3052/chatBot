04e2cc96f9e3b2f0c422273d5a56aa1c06ba815a
367 <367@tuta.io>
2025-09-07 00:05:57 -0500

    new file:   internal/2025-09-06/gemini_test.go

--- internal/2025-09-06/gemini.go
+++ internal/2025-09-06/gemini.go
@@ -1,22 +1,21 @@
-package main
+package gemini
 
 import (
    "bytes"
    "encoding/json"
    "net/http"
-   "os"
 )
-
-type part struct {
-   Text string `json:"text"`
-}
 
 type content struct {
    Role  string `json:"role"`
    Parts []part `json:"parts"`
 }
 
-func main() {
+type part struct {
+   Text string `json:"text"`
+}
+
+func generate(text string) (*http.Response, error) {
    var value struct {
       Contents []content `json:"contents"`
       GenerationConfig struct {
@@ -32,27 +31,23 @@ func main() {
       {
          Role: "user",
          Parts: []part{
-            {Text: "2+3"},
+            {Text: text},
          },
       },
    } 
    value.GenerationConfig.ThinkingConfig.ThinkingBudget = -1
    data, err := json.Marshal(value)
    if err != nil {
-      panic(err)
+      return nil, err
    }
    req, err := http.NewRequest(
       "POST", "https://generativelanguage.googleapis.com", bytes.NewReader(data),
    )
+   if err != nil {
+      return nil, err
+   }
    req.URL.Path = "/v1beta/models/gemini-2.5-pro:streamGenerateContent"
    req.URL.RawQuery = "key=${GEMINI_API_KEY}"
    req.Header.Set("content-type", "application/json")
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      panic(err)
-   }
-   err = resp.Write(os.Stdout)
-   if err != nil {
-      panic(err)
-   }
+   return http.DefaultClient.Do(req)
 }
